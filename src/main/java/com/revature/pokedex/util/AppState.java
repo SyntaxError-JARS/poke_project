f433651ef88f362330c35afba93343996180a1b3
JesterCharles <JesterCharles@gmail.com>
2022-05-06 12:18:18 -0400

    Added a logger into the app state, showcase Dependency injection for the logger to pass to other files

--- src/main/java/com/revature/pokedex/util/AppState.java
+++ src/main/java/com/revature/pokedex/util/AppState.java
@@ -3,6 +3,7 @@ package com.revature.pokedex.util;
 import com.revature.pokedex.menus.RegisterMenu;
 import com.revature.pokedex.menus.WelcomeMenu;
 import com.revature.pokedex.services.TrainerServices;
+import com.revature.pokedex.util.logging.Logger;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,23 +13,27 @@ public class AppState {
     private static boolean isRunning;
     private WelcomeMenu welcomeMenu;
     private RegisterMenu registerMenu;
+    private final Logger logger;
     // once we add register we will need private final MenuRouter router;
 
     public AppState() {
-        System.out.println("2. Generating instance of AppState.");
+
+        logger = Logger.getLogger(true);
+
+        logger.log("2. Generating instance of AppState.");
         isRunning = true;
         BufferedReader terminalReader = new BufferedReader(new InputStreamReader(System.in));
         TrainerServices trainerServices = new TrainerServices();
 
         // TODO: Why are we doing all of this!?
-        this.welcomeMenu = new WelcomeMenu(terminalReader, trainerServices);
+        this.welcomeMenu = new WelcomeMenu(terminalReader, trainerServices, logger);
         this.registerMenu = new RegisterMenu(terminalReader);
     }
 
     public void startup(){
         try {
             while(isRunning) {
-                System.out.println("Application successfully started");
+                logger.info("Application successfully started");
                 // registerMenu.render();
                 welcomeMenu.render(); // comment in and out based on what you want to use
             }
@@ -39,7 +44,6 @@ public class AppState {
 
     public static void shutdown(){
         isRunning = false;
-        System.out.println("Application shutting down...");
     }
 
 }
