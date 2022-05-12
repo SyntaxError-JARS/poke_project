cb593838264cb720e1f0f3114f9f988cf98d18c7
JesterCharles <JesterCharles@gmail.com>
2022-05-12 03:21:01 -0400

    Updated additional servlet skeletons

--- src/main/java/com/revature/pokedex/web/servlets/TrainerServlet.java
+++ src/main/java/com/revature/pokedex/web/servlets/TrainerServlet.java
@@ -4,19 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.pokedex.daos.TrainerDao;
 import com.revature.pokedex.models.Trainer;
 import com.revature.pokedex.services.TrainerServices;
+import com.revature.pokedex.util.collections.List;
+import com.revature.pokedex.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
 
 // @WebServlet("/trainers")
 public class TrainerServlet extends HttpServlet {
 
     private final TrainerServices trainerServices;
     private final ObjectMapper mapper;
+    private final Logger logger = Logger.getLogger();
 
     public TrainerServlet(TrainerServices trainerServices, ObjectMapper mapper) {
         this.trainerServices = trainerServices;
@@ -25,10 +30,17 @@ public class TrainerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Trainer[] trainers = trainerServices.readAll();
 
+        Trainer[] trainers = trainerServices.readAll();
         String payload = mapper.writeValueAsString(trainers);
 
         resp.getWriter().write(payload);
     }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+
 }
